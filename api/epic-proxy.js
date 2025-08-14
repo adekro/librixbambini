7f5716fb50cca3f4ee6fa2238fc6caa0180ef434
adekro <33175405+adekro@users.noreply.github.com>
2025-08-14 16:11:59 +0200

    Update epic-proxy.js

--- api/epic-proxy.js
+++ api/epic-proxy.js
@@ -1,32 +1,23 @@
-import express from 'express';
 import fetch from 'node-fetch';
-import cors from 'cors';
 
-const app = express();
-app.use(cors());
-
-// All requests to /api/epic-proxy are handled by this function
-app.get('/*', async (req, res) => {
-  const targetUrl = 'https://www.getepic.com';
-
+export default async function handler(req, res) {
   try {
+    const targetUrl = 'https://www.getepic.com';
+
     const response = await fetch(targetUrl, {
-      headers: { 'User-Agent': 'Mozilla/5.0' }
+      headers: { 'User-Agent': 'Mozilla/5.0' },
     });
 
     let html = await response.text();
 
-    // Remove security meta tags
+    // Rimuovi meta di sicurezza
     html = html.replace(/<meta[^>]*http-equiv=["']?Content-Security-Policy["']?[^>]*>/gi, '');
     html = html.replace(/<meta[^>]*http-equiv=["']?X-Frame-Options["']?[^>]*>/gi, '');
 
     res.setHeader('Content-Type', 'text/html');
     res.send(html);
-
   } catch (err) {
     console.error(err);
-    res.status(500).send('Proxy Error: ' + err.message);
+    res.status(500).send('Errore nel proxy: ' + err.message);
   }
-});
-
-export default app;
+}
