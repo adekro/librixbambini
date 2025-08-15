7067eaf63e2070574500ee8e1ad23de20d317cbd
adekro <33175405+adekro@users.noreply.github.com>
2025-08-15 09:15:55 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Imposta il base path a '/' per assicurare che gli asset
-  // vengano caricati correttamente su Vercel.
-  base: '/',
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5173',
+        changeOrigin: true
+      }
+    }
+  }
 });
